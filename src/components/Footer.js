53bf3c80d1beb3103a8a00414267a92cffcd8bda
danielwong0115 <114831407+danielwong0115@users.noreply.github.com>
2024-08-01 17:51:20 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ const Footer = () => (
         <a class="navbar-brand" href="/">321Origami</a>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="https://www.facebook.com/"><img class="img-fluid mx-auto d-block" src={require("../assets/images/facebook2.jpg")} alt="Facebook Logo" /></a>
+            <a class="nav-link" href="https://www.facebook.com/"><img class="img-fluid mx-auto d-block" src="../assets/images/facebook2.jpg" alt="Facebook Logo" /></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="https://www.instagram.com/"><img class="img-fluid mx-auto d-block" src={require("../assets/images/instagram3.jpg")} alt="Instagram Logo" /></a>
+            <a class="nav-link" href="https://www.instagram.com/"><img class="img-fluid mx-auto d-block" src="../assets/images/instagram3.jpg" alt="Instagram Logo" /></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="https://x.com/"><img class="img-fluid mx-auto d-block" src={require("../assets/images/twitter1.png")} alt="Twitter Logo" /></a>
+            <a class="nav-link" href="https://x.com/"><img class="img-fluid mx-auto d-block" src="../assets/images/twitter1.png" alt="Twitter Logo" /></a>
           </li>
         </ul>
       </div>
