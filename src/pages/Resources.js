da3dd2a255489dcdcfd47a6dbdfd15f632df68a7
danielwong0115 <114831407+danielwong0115@users.noreply.github.com>
2024-08-01 18:21:08 -0700

    Update Resources.js

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,45 +1,47 @@
 import React from 'react';
 
 const Resources = () => (
-    <section>
-        <div class="container-lg my-5">
-            <div class="row">
-            <div class="col">
-                <div class="p-4">
-                <h1>Resources</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Tincidunt dui ut ornare lectus sit amet est placerat. Et netus et malesuada fames ac turpis egestas. Volutpat est velit egestas dui.</p>
+    <main class="min-vh-100">
+        <section>
+            <div class="container-lg my-5">
+                <div class="row">
+                <div class="col">
+                    <div class="p-4">
+                    <h1>Resources</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Tincidunt dui ut ornare lectus sit amet est placerat. Et netus et malesuada fames ac turpis egestas. Volutpat est velit egestas dui.</p>
+                    </div>
+                </div>
+                </div>
+                <div class="row">
+                <div class="col">
+                    <div class="p-4">
+                    <h2>Origami.org</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Go to: <a class = "text-dark" href="https://www.origami.org/">https://www.origami.org</a></p>
+                    </div>
+                </div>
+                </div>
+                <div class="row">
+                <div class="col">
+                    <div class="p-4">
+                    <h2>Origami-make.org</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Go to: <a class = "text-dark" href="https://www.origami-make.org/">https://www.origami-make.org</a></p>
+                    </div>
+                </div>
+                </div>
+                <div class="row">
+                <div class="col">
+                    <div class="p-4">
+                    <h2>The Origami Papershop</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Go to: <a class = "text-dark" href="https://www.theorigamipapershop.com/">https://www.origami-make.org</a></p>
+                    </div>
+                </div>
                 </div>
             </div>
-            </div>
-            <div class="row">
-            <div class="col">
-                <div class="p-4">
-                <h2>Origami.org</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p>Go to: <a class = "text-dark" href="https://www.origami.org/">https://www.origami.org</a></p>
-                </div>
-            </div>
-            </div>
-            <div class="row">
-            <div class="col">
-                <div class="p-4">
-                <h2>Origami-make.org</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p>Go to: <a class = "text-dark" href="https://www.origami-make.org/">https://www.origami-make.org</a></p>
-                </div>
-            </div>
-            </div>
-            <div class="row">
-            <div class="col">
-                <div class="p-4">
-                <h2>The Origami Papershop</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p>Go to: <a class = "text-dark" href="https://www.theorigamipapershop.com/">https://www.origami-make.org</a></p>
-                </div>
-            </div>
-            </div>
-        </div>
-    </section>
+        </section>
+    </main>
   );
   
   export default Resources;
